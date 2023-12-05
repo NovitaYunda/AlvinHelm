b6e0b65c54253e44c5f265b70e9ac555926d21d3
Algoritma Dwi Febri Fathiyakan <icoreforce87@gmail.com>
2023-12-05 14:01:09 +0700

    menambahklan halaman pilihan login

---
+++ pilihan.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- SITE TITTLE -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login Admin</title>
+  
+  <!-- FAVICON -->
+  <link href="assets/img/alvin real1.png" rel="shortcut icon">
+  <!-- PLUGINS CSS STYLE -->
+  <!-- <link href="../assets/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet"> -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap-slider.css">
+  <!-- Font Awesome -->
+  <link href="../assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Owl Carousel -->
+  <link href="../assets/plugins/slick-carousel/slick/slick.css" rel="stylesheet">
+  <link href="../assets/plugins/slick-carousel/slick/slick-theme.css" rel="stylesheet">
+  <!-- Fancy Box -->
+  <link href="../assets/plugins/fancybox/jquery.fancybox.pack.css" rel="stylesheet">
+  <link href="../assets/plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+  <!-- CUSTOM CSS -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="../assets/sweeetalert2/dist/sweetalert2.min.css">
+  <script src="../assets/sweeetalert2/dist/sweetalert2.all.min.js"></script>
+
+  <style>
+    body {
+      background-image: url('assets/img/MOTO11.png'); /* Ganti dengan path gambar yang ingin digunakan */
+      background-size: cover;
+      background-position: center;
+    }
+  </style>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+
+<body class="body-wrapper">
+
+<section class="login border-top-1 text-center py-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-9 align-item-center">
+                <div class="border text-center bg-white rounded">
+                    <h3 class="bg-gray p-4">Login</h3>
+                    <form method="POST">
+                    <fieldset class="p-4 d-inline-block">
+                    <img src="assets/img/alvin real1.png" width="200" height="200"class="rounded mx-auto d-block" alt="..."><br><br>
+                            <a href="admin/index.php" class="btn btn-primary text-white" style="width: 200px"><b>Login Admin</b></a><br><br>
+                            <a href="login.php" class="btn btn-primary custom-btn text-white" style="width: 200px"><b>Login Customer</b></a>
+                        </fieldset>
+                        <a class="mt-3 d-block text-primary" href="index.php">Kembali ke beranda</a><br><br>
+                      <style>
+                        .custom-btn {
+                        margin-rigth: 30px;/* Atur jarak horizontal antara tombol */
+                        }
+                      </style>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- JAVASCRIPTS -->
+<script src="../assets/plugins/jQuery/jquery.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap-slider.js"></script>
+  <!-- tether js -->
+<script src="../assets/plugins/tether/js/tether.min.js"></script>
+<script src="../assets/plugins/raty/jquery.raty-fa.js"></script>
+<script src="../assets/plugins/slick-carousel/slick/slick.min.js"></script>
+<script src="../assets/plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+<script src="../assets/plugins/fancybox/jquery.fancybox.pack.js"></script>
+<script src="../assets/plugins/smoothscroll/SmoothScroll.min.js"></script>
+<!-- google map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+<script src="../assets/plugins/google-map/gmap.js"></script>
+<script src="../assets/js/script.js"></script>
+
+
+<!-- Page level plugins -->
+<script src="../assets/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+<script src="../assets/js/demo/datatables-demo.js"></script>
+
+</body>
+
+
+</html>
